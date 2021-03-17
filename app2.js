af0ec110bf4cc94acbdb989ad89192f41f5b6fe1
Madhuarla <madhuarla3@gmail.com>
2021-03-17 01:59:35 -0500

    app2.js table created

--- app2.js
+++ app2.js
@@ -9,11 +9,13 @@ const server =
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
-      res.write('<html> <head> <title> Served </title> </head>')
+      res.write('<html> <head> <title> Tables- nodejs </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
+      res.write('<table>  <tr> <td> <th>Location</th> </td> <td>  <th>Number of hours</th> </td> <td><th>Amount of Money</th> </td> </tr>')
+      res.write('<tr><td>&nbsp;</td><td>Key West, Florida</td> <td>&nbsp;</td><td>6Hrs</td><td>&nbsp;</td><td>1000$</td></tr>')
+      res.write('<tr> <td>&nbsp;</td> <td>Las Vegas, Nevada</td><td>&nbsp;</td><td>2days </td> <td>&nbsp;</td> <td>1500$ </td></tr>')
+      res.write(' <tr><td>&nbsp;</td> <td>Disneyland, CL</td> <td>&nbsp;</td> <td>12Hrs</td> <td>&nbsp;</td> <td>500$</td> </tr>')  
+      res.write('<tr><td>&nbsp;</td><td>Newyork Times Square</td><td>&nbsp;</td> <td>2Hrs</td> <td>&nbsp;</td> <td>300$</td> </tr></table>')
       res.end('</body></html>')
       //Close the response
     }                           
